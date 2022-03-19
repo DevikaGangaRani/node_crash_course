fe17cd4785562cb493642143d89289d1b3df4718
DevikaGangaRani <devikarani2001@gmail.com>
2022-03-19 23:19:15 +0530

    Initial commit

---
+++ person.js
@@ -0,0 +1,16 @@
+// Module WRAPPER Function
+// (function (exports, require, module, __filename, __dirname) {});
+
+// console.log(__dirname, __filename);
+
+class person {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+
+  greeting() {
+    console.log(`My name is ${this.name} and i am ${this.age}`);
+  }
+}
+module.exports = person;
